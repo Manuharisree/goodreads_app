65173c73c8159c6c1c63883dc3ee878a733d4860
Manu <manu@freshdesk.com>
2016-07-15 14:25:30 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodreadsApp::Application.config.secret_token = '6c5f8bbcabd14946197db6b8b7c7cf6606552274eaba47011cf3678fcf47c81abe3fc5bfa30a2e8c68fd77376bd2a5ae724ccabc8877792c7b6d148b02696585'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GoodreadsApp::Application.config.session_store :cookie_store, key: '_goodreads_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GoodreadsApp::Application.config.session_store :active_record_store
